c2b6f5fd47409bbd15dceed10e3cb2d2d3455034
gokhanKaratass <gokhankaratas650@gmail.com>
2022-08-11 02:11:18 +0300

    Update add todo component

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { usePostTodoMutation } from "../../store/apis/TheTodoListApi";
 
 export default function AddTodo() {
-  return <div>AddTodo</div>;
+  const [postRule] = usePostTodoMutation();
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddRule = () => {
+    if (inputValue.length > 0) {
+      let body = {
+        id: Math.random(),
+        name: inputValue,
+        status: "unselect",
+      };
+      postRule(body);
+    }
+  };
+  return (
+    <div className="add-todo-wrapper">
+      <div className="add-todo-container">
+        <form className="add-todo-block">
+          <label>
+            <input
+              placeholder="New Task"
+              type="text"
+              name="name"
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          </label>
+          <input
+            className="add-button"
+            type="submit"
+            value="Add"
+            onClick={handleAddRule}
+          />
+        </form>
+      </div>
+    </div>
+  );
 }

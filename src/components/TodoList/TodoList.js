108e890ef25bec4e1dba0ca22d34118fdcb746b4
gokhanKaratass <gokhankaratas650@gmail.com>
2022-08-11 02:10:48 +0300

    Update todo list component

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,10 +1,22 @@
 import React from "react";
+
 import TodoListItem from "../TodoList/TodoListItem";
 
+import { useGetTheTodoListApiQuery } from "../../store/apis/TheTodoListApi";
+
 export default function TodoList() {
+  const { data } = useGetTheTodoListApiQuery();
+
+  console.log("data", data);
+
   return (
-    <div>
-      <TodoListItem />
+    <div className="todo-list-wrapper">
+      <div className="todo-list-container">
+        {data &&
+          data.map((item, index) => {
+            return <TodoListItem data={item} key={index} />;
+          })}
+      </div>
     </div>
   );
 }
